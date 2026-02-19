d2c6c8fea88f48e2b69d7a09896f8c391af1b743
Sanjay Sakthivel <sanjaysakthivel2000@gmail.com>
2026-02-19 13:32:24 -0600

    completed tree video 1

--- 2_tree/src/BST.java
+++ 2_tree/src/BST.java
@@ -33,10 +33,27 @@ public class BST {
     }
 
     void insert(int val){
-        insert(val,root);
+        root=insert(val,root);
     }
 
-    Node insert(int val, Node root){
+    void populate(int[] nums){
+        for (int num : nums) {
+            insert(num);
+        }
+    }
+
+    void populateSorted(int[] nums, int start, int end){
+        if(start>end){
+            return;
+        }
+
+        int mid=start+(end-start)/2;
+        insert(nums[mid]);
+        populateSorted(nums,start,mid-1);
+        populateSorted(nums,mid+1,end);
+    }
+
+    private Node insert(int val, Node root){
         if(root==null){
             return new Node(val);
         }
@@ -58,11 +75,71 @@ public class BST {
         return isBalanced(root);
     }
 
-    boolean isBalanced(Node root){
+    private boolean isBalanced(Node root){
         if(root==null){
             return true;
         }
 
         return Math.abs(getHeight(root.left)-getHeight(root.right))<=1 && isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    void display(){
+        display(root,"");
+    }
+
+    private void display(Node root, String detail){
+        if(root==null){
+            return;
+        }
+
+        System.out.println(detail+ root.val);
+        display(root.left,"Left Child of "+root.val+":");
+        display(root.right,"right Child of "+root.val+":");
+    }
+
+    void preorder_display(){
+        System.out.println("Preorder: ");
+        preorder_display(root);
+        System.out.println();
+    }
+
+    private void preorder_display(Node root){
+        if(root==null){
+            return;
+        }
+        System.out.print(root.val+" ");
+        preorder_display(root.left);
+        preorder_display(root.right);
+    }
+
+    void inorder_display(){
+        System.out.println("Inorder: ");
+        inorder_display(root);
+        System.out.println();
+    }
+
+    private void inorder_display(Node root){
+        if(root==null){
+            return;
+        }
+        inorder_display(root.left);
+        System.out.print(root.val+" ");
+        inorder_display(root.right);
+    }
+
+
+    void postorder_display(){
+        System.out.println("Postorder: ");
+        postorder_display(root);
+        System.out.println();
+    }
+
+    private void postorder_display(Node root){
+        if(root==null){
+            return;
+        }
+        inorder_display(root.left);
+        inorder_display(root.right);
+        System.out.print(root.val + " ");
     }
 }
--- 2_tree/src/MakeTree.java
+++ 2_tree/src/MakeTree.java
@@ -1,8 +1,16 @@
 public class MakeTree {
     public static void main(String[] args) {
-        Tree tree=new Tree();
-        Node root=tree.createTree();
-        tree.display(root,"");
-        tree.displayPretty(root,1);
+//        Tree tree=new Tree();
+//        Node root=tree.createTree();
+//        tree.display(root,"");
+//        tree.displayPretty(root,1);
+
+        BST bst=new BST();
+        int[] nums={1,2,3,4,5,6,7,8,9};
+        bst.populateSorted(nums,0,nums.length-1);
+        bst.display();
+        bst.inorder_display();
+        bst.postorder_display();
+        bst.preorder_display();
     }
 }
